f4adaf300d93875d7bed659f84d17ae93cf1ef4c
kultanensami-boop <kultanen.sami@gmail.com>
2026-04-16 19:19:01 +0300

    Add Event model with relevant fields

--- app/models/event.py
+++ app/models/event.py
@@ -1,1 +1,14 @@
+from sqlalchemy import Column, Integer, String, DateTime, JSON, ForeignKey
+from sqlalchemy.sql import func
+from app.database import Base
+
+class Event(Base):
+    __tablename__ = "events"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, index=True)  # myöhemmin ForeignKey
+    timestamp = Column(DateTime(timezone=True), server_default=func.now())
+    categories = Column(JSON, nullable=False)
+    comment = Column(String, nullable=True)
+    url = Column(String, nullable=True)
 
