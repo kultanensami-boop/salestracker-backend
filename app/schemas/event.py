e6ce368c3c7475e397dd2612275645ffd72835e3
kultanensami-boop <kultanen.sami@gmail.com>
2026-04-16 19:19:20 +0300

    Add EventCreate and EventResponse models

--- app/schemas/event.py
+++ app/schemas/event.py
@@ -1,1 +1,17 @@
+from pydantic import BaseModel
+from typing import List, Optional
+from datetime import datetime
+
+class EventCreate(BaseModel):
+    categories: List[str]
+    comment: Optional[str] = None
+    url: Optional[str] = None
+
+class EventResponse(EventCreate):
+    id: int
+    user_id: int
+    timestamp: datetime
+
+    class Config:
+        orm_mode = True
 
